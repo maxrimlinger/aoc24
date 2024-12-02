9370df624b4ec9fbdfd2e1af636cb9e3b0f69ebd
Max Rimlinger <37249275+maxrimlinger@users.noreply.github.com>
2024-12-01 21:48:04 -0500

    solve day 1

---
+++ day1/day1.py
@@ -0,0 +1,16 @@
+with open("input.txt", "r") as file:
+    both_lists = file.read().split()
+    list1 = []
+    list2 = []
+    for i, n in enumerate(both_lists):
+        if i % 2 == 0:
+            list1.append(n)
+        else:
+            list2.append(n)
+    list1.sort()
+    list2.sort()
+    total_diff = 0
+    for n1, n2 in list(zip(list1, list2)):
+        # by now you might be thinking "this is incredibly inefficient" and you'd be right
+        total_diff += abs(int(n1)-int(n2))
+    print(total_diff)
