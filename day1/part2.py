b1944c046a58b6bdfab343a3601e7fe209a148ae
Max Rimlinger <37249275+maxrimlinger@users.noreply.github.com>
2024-12-01 21:56:38 -0500

    solve part 2

--- day1/part2.py
+++ day1/part2.py
@@ -1,1 +1,20 @@
-# stubbed
+with open("input.txt", "r") as file:
+    both_lists = file.read().split()
+    list1 = []
+    list2 = []
+    for i, n in enumerate(both_lists):
+        if i % 2 == 0:
+            list1.append(n)
+        else:
+            list2.append(n)
+    freq_map = dict()
+    for n in list2:
+        if n in freq_map:
+            freq_map[n] += 1
+        else:
+            freq_map[n] = 1
+    total_sim = 0
+    for n in list1:
+        if n in freq_map:
+            total_sim += int(n) * freq_map[n]
+    print(total_sim)
